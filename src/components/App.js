a24b70ee99f09d1a524876eddefd10a20e8600e1
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2024-01-14 14:41:32 +0000

    first

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,12 @@ const App = () => {
   return (
     <div id="main">
                {/* Do not remove the main div */}
+               <ol key={"relativeList"}>
+                <li key={"relativeListItem1"}>Mama</li>
+                <li key={"relativeListItem2"}>Chacha</li>
+                <li key={"relativeListItem3"}>Nana</li>
+                <li key={"relativeListItem4"}>Dada</li>
+               </ol>
     </div>
   )
 }
